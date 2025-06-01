0c957c6a71a9cdb4c3a3785dc9dbe7e3794d225f
Chetan0402 <chetansharma67g@gmail.com>
2025-06-01 16:23:05 +0530

    Setup Docker for deployment

--- internal/veripass.go
+++ internal/veripass.go
@@ -1,5 +1,23 @@
 package veripass
 
-func Run() {
+import (
+	"net/http"
 
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+)
+
+func Run() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /ping", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("pong"))
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+	})
+	err := http.ListenAndServe("0.0.0.0:8000", h2c.NewHandler(mux, &http2.Server{}))
+	if err != nil {
+		panic(err)
+	}
 }
