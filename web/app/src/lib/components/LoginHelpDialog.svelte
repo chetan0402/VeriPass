1b22af79f51e83c488559599a823e074941b5674
anuj-softech <179094956+anuj-softech@users.noreply.github.com>
2025-06-10 01:00:27 +0530

    feat: add login help dialog and improve UI design
    - Implemented a help dialog for login issues to assist users
    - Enhanced UI elements

---
+++ web/app/src/lib/components/LoginHelpDialog.svelte
@@ -0,0 +1,37 @@
+<script>
+	import { CloseOutline } from 'flowbite-svelte-icons';
+	export let onClose = () => {};
+</script>
+
+<div
+	class="relative m-10 flex h-[max(200px,70vh)] w-[clamp(100px,80vw,600px)] flex-col items-center justify-around rounded-2xl border-3 border-[#5555C2] bg-white p-5"
+>
+	<div class="flex flex-row items-center justify-center">
+		<h1 class="text-secondary text-2xl font-bold">Login Help</h1>
+	</div>
+	<div class="bg-primary-100 mt-2 space-y-4 overflow-y-auto rounded-md p-4 text-sm text-gray-700">
+		<div>
+			<p class="font-semibold">Q. I don’t have my Institute mail ID.</p>
+			<p>
+				A. Please check the email ID provided by you during admission. If you haven't received any
+				email about your institute ID, contact the Central Computing Facility (CCF), MANIT.
+			</p>
+		</div>
+		<div>
+			<p class="font-semibold">Q. I forgot the password of my email ID.</p>
+			<p>
+				A. Try resetting your password using your phone number. If that doesn’t work, contact CCF,
+				MANIT for assistance.
+			</p>
+		</div>
+		<div>
+			<p class="font-semibold">Q. I can’t find my account in the Google sign-in list.</p>
+			<p>A. Make sure your student Gmail account is logged in on the browser you are using.</p>
+		</div>
+	</div>
+	<p class="text-secondary-700 mt-2 text-sm">Issue not listed here?</p>
+	<button class="bg-secondary w-full rounded-full p-3 text-white">Send Mail to Admin</button>
+	<button on:click={onClose} class="text-secondary absolute top-5 right-5">
+		<CloseOutline />
+	</button>
+</div>
--- web/app/src/routes/login/+page.svelte
+++ web/app/src/routes/login/+page.svelte
@@ -1,38 +1,55 @@
 <script lang="ts">
-	import { Card, Button } from 'flowbite-svelte';
 	import { Toast } from 'flowbite-svelte';
 	import { ExclamationCircleOutline, QuestionCircleOutline } from 'flowbite-svelte-icons';
 	import { blur } from 'svelte/transition';
 	import GoogleButton from '$lib/components/GoogleButton.svelte';
+	import LoginHelpDialog from '$lib/components/LoginHelpDialog.svelte';
+
+	let help_dialog = $state('hidden');
+
+	function toggleHelpDialog() {
+		help_dialog = help_dialog === 'hidden' ? 'flex' : 'hidden';
+	}
 </script>
 
 <div
 	class="animate__animated animate__fadeIn flex h-dvh w-dvw flex-col items-center justify-center"
 >
-	<img src="logo.png" class="w52 h-52" alt="logo" />
-	<p class="text-primary text-4xl font-bold">VeriPass</p>
-	<p class="text-base">Student Login</p>
+	<img src="logo.png" class="h-40 w-40" alt="logo" />
+	<p class="text-primary text-3xl font-bold">VeriPass</p>
+	<p class="text-secondary-600 mt-10 text-3xl font-bold">
+		<span class="text-primary-500">Student</span> Login
+	</p>
 
-	<Card
-		class="mt-10 mb-10 flex h-full flex-col items-center justify-center rounded-3xl bg-gradient-to-b from-[#5555C2] to-[#B66AE7] p-6"
+	<div
+		class="mt-5 flex h-full w-dvw flex-col items-center justify-center bg-[url('/wave-bg.svg')] bg-cover bg-top bg-no-repeat pt-10"
 	>
 		<p class="text-center text-2xl font-bold text-white">Login with <br /> institute's Email ID</p>
 		<GoogleButton className="scale-125 mt-10" />
+		<button
+			onclick={toggleHelpDialog}
+			class="w- mt-6 flex h-[40px] flex-row items-center justify-center rounded-full border-2 pr-[24px] pl-[12px] text-[12px] text-white"
+		>
+			<QuestionCircleOutline class="m-[8px]" />
+			need help
+		</button>
 		<Toast
 			dismissable={false}
 			transition={blur}
 			params={{ amount: 50, delay: 20 }}
-			class="bg-primary-50 mt-10 transform"
+			class="bg-primary-50 mt-10 mb-10 transform"
 		>
 			{#snippet icon()}
 				<ExclamationCircleOutline class="text-primary-500 dark:bg-primary-800 h-6  w-6"
 				></ExclamationCircleOutline>
 			{/snippet}
-			Only login with scholar_number@stu.manit.ac.in Google ID
+			Only login with @stu.manit.ac.in Google ID
 		</Toast>
-		<Button color="alternative" class="mt-5 text-white">
-			<QuestionCircleOutline class="mr-1" />
-			need help
-		</Button>
-	</Card>
+	</div>
+
+	<div
+		class="{help_dialog} absolute z-10 h-dvh w-dvw flex-row items-center justify-center bg-[#000000aa] backdrop-blur-2xl"
+	>
+		<LoginHelpDialog onClose={toggleHelpDialog} />
+	</div>
 </div>
