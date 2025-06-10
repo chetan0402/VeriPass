dbab9d6bc4ec0b295f50c3397baaa74c27ab5b79
Anuj Shrivastava <179094956+anuj-softech@users.noreply.github.com>
2025-06-10 21:38:45 +0530

    splash and login ui created (design only) (#1)
    * splash and login ui
    
    * created splash and login ui
    
    * fix: remove unused parameters and commented code
    
    * feat: add login help dialog and improve UI design
    
    - Implemented a help dialog for login issues to assist users
    - Enhanced UI elements
    
    * fix: optimized code and migrated script to use svelte 5 runes in ui components and pages
    
    * fix: use goto instead of href, proper types in props
    
    ---------
    
    Co-authored-by: Chetan0402 <chetansharma67g@gmail.com>

--- web/app/src/routes/+page.svelte
+++ web/app/src/routes/+page.svelte
@@ -1,30 +1,67 @@
 <script lang="ts">
 	import { transport } from '$lib';
-	import { UserService, type User } from '$lib/gen/veripass/v1/user_pb';
+	import { type User, UserService } from '$lib/gen/veripass/v1/user_pb';
 	import { createClient } from '@connectrpc/connect';
 	import { onMount } from 'svelte';
+	import { Progressbar } from 'flowbite-svelte';
+	import { goto } from '$app/navigation';
 
+	let status_message: string = $state<string>('Getting things ready...');
 	let user = $state<User>();
-	$inspect(user);
+
+	let maxProgress: number = $state<number>(0);
+	let progress: number = $state<number>(0);
+
+	$effect(() => {
+		if (progress < maxProgress) {
+			const id = setTimeout(() => {
+				progress = progress + 1;
+			}, 10);
+			return () => clearTimeout(id);
+		}
+	});
 
 	const client = createClient(UserService, transport);
 
+	function openNextScreen(user: User) {
+		status_message = 'Welcome ' + user.name + '!';
+	}
+
+	function isUserLoggedIn() {
+		return false;
+	}
+
+	function getUserID() {
+		return '12345';
+	}
+
+	function openLoginScreen() {
+		status_message = 'Taking you to the login page...';
+		setTimeout(() => {
+			goto('/login');
+		}, 1600);
+	}
+
 	onMount(async () => {
-		try {
-			const response = await client.getUser({ id: '12345' });
-			user = response;
-		} catch (error) {
-			console.error('Error fetching user data:', error);
+		maxProgress = 80;
+		if (isUserLoggedIn()) {
+			try {
+				user = await client.getUser({ id: getUserID() });
+				maxProgress = 100;
+				openNextScreen(user);
+			} catch (error) {
+				console.error('Error fetching user data:', error);
+			}
+		} else {
+			maxProgress = 100;
+			openLoginScreen();
 		}
 	});
 </script>
 
-<h1>Welcome to VeriPass</h1>
-
-{#if user}
-	<div>
-		<p><strong>ID:</strong> {user.id}</p>
-	</div>
-{:else}
-	<p>Loading user data...</p>
-{/if}
+<div class="flex h-dvh w-dvw flex-col items-center justify-center">
+	<img src="logo.png" class="animate__fadeIn animate__animated h-72 w-72" alt="logo" />
+	<h1 class="text-primary animate__fadeIn animate__animated text-5xl font-bold">VeriPass</h1>
+	<Progressbar class="mt-32 w-64" {progress} />
+	<p class="mt-5 font-normal dark:text-white">{status_message}</p>
+</div>
