f816e9e483880aa9c3cf93e2c6c653ea60b40b2b
Chetan0402 <chetansharma67g@gmail.com>
2025-06-20 18:23:52 +0530

    Implement: Admin Service

---
+++ internal/services/admin/adminservice.go
@@ -0,0 +1,49 @@
+package adminservice
+
+import (
+	"context"
+
+	"connectrpc.com/connect"
+	"github.com/chetan0402/veripass/internal/ent"
+	"github.com/chetan0402/veripass/internal/ent/admin"
+	veripassv1 "github.com/chetan0402/veripass/internal/gen/veripass/v1"
+	"github.com/chetan0402/veripass/internal/gen/veripass/v1/veripassv1connect"
+)
+
+type AdminService struct {
+	client *ent.Client
+}
+
+var _ veripassv1connect.AdminServiceHandler = (*AdminService)(nil)
+
+func New(client *ent.Client) *AdminService {
+	return &AdminService{
+		client: client,
+	}
+}
+
+// GetAdmin implements veripassv1connect.AdminServiceHandler.
+func (s *AdminService) GetAdmin(ctx context.Context, r *connect.Request[veripassv1.GetAdminRequest]) (*connect.Response[veripassv1.Admin], error) {
+	var (
+		email = r.Msg.Email
+	)
+
+	admin, err := s.client.Admin.Query().Where(admin.Email(email)).First(ctx)
+	if err != nil {
+		if ent.IsNotFound(err) {
+			return nil, connect.NewError(connect.CodeNotFound, err)
+		}
+		return nil, err
+	}
+
+	return connect.NewResponse(toProto(admin)), nil
+}
+
+func toProto(admin *ent.Admin) *veripassv1.Admin {
+	return &veripassv1.Admin{
+		Email:      admin.Email,
+		Name:       admin.Name,
+		Hostel:     admin.Hostel,
+		CanAddPass: admin.CanAddPass,
+	}
+}
