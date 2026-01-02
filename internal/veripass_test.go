7e6bbe0f5b5101f0813a38a449fad99f647fdefa
Chetan0402 <chetansharma67g@gmail.com>
2026-01-02 22:44:28 +0530

    improve clarity of testing by introducing failIfNotEqual

--- internal/veripass_test.go
+++ internal/veripass_test.go
@@ -70,163 +70,142 @@ func TestMain(t *testing.T) {
 	attest(t, err)
 
 	dexClient := dex.NewDexClient(conn)
-	mockUser, mockAdmin := getUserAdminPair(t, dbClient, dexClient, true)
+	t.Run("Happy path", func(t *testing.T) {
+		student, admin := getUserAdminPair(t, dbClient, dexClient, true)
 
-	studentClient := getClient(t, mockUser.Id+"@stu.manit.ac.in")
-	adminClient := getClient(t, mockAdmin.Email)
+		sc := getClient(t, student.Id+"@stu.manit.ac.in")
+		ac := getClient(t, admin.Email)
 
-	studentUserClient := veripassv1connect.NewUserServiceClient(studentClient, HOST)
-	studentPassClient := veripassv1connect.NewPassServiceClient(studentClient, HOST)
+		studentUserClient := veripassv1connect.NewUserServiceClient(sc, HOST)
+		studentPassClient := veripassv1connect.NewPassServiceClient(sc, HOST)
 
-	adminAdminClient := veripassv1connect.NewAdminServiceClient(adminClient, HOST)
+		adminAdminClient := veripassv1connect.NewAdminServiceClient(ac, HOST)
 
-	mockPass1 := veripassv1.Pass{
-		UserId: mockUser.Id,
-		Type:   veripassv1.Pass_PASS_TYPE_CLASS,
-	}
+		mockPass1 := veripassv1.Pass{
+			UserId: student.Id,
+			Type:   veripassv1.Pass_PASS_TYPE_CLASS,
+		}
 
-	mockPass2 := veripassv1.Pass{
-		UserId: mockUser.Id,
-		Type:   veripassv1.Pass_PASS_TYPE_HOME,
-	}
+		mockPass2 := veripassv1.Pass{
+			UserId: student.Id,
+			Type:   veripassv1.Pass_PASS_TYPE_HOME,
+		}
 
-	publicKey, err := adminAdminClient.GetPublicKey(ctx, connect.NewRequest(&emptypb.Empty{}))
-	attest(t, err)
+		publicKey, err := adminAdminClient.GetPublicKey(ctx, connect.NewRequest(&emptypb.Empty{}))
+		attest(t, err)
 
-	user, err := studentUserClient.GetUser(ctx, connect.NewRequest(&veripassv1.GetUserRequest{
-		Id: nil,
-	}))
-	attest(t, err)
-	if !proto.Equal(user.Msg, mockUser) {
-		t.Fatal("User response not equal")
-	}
+		user, err := studentUserClient.GetUser(ctx, connect.NewRequest(&veripassv1.GetUserRequest{
+			Id: nil,
+		}))
+		attest(t, err)
+		failIfNotEqual(t, user.Msg, student)
 
-	exitResponse, err := studentUserClient.Exit(ctx, connect.NewRequest(&veripassv1.ExitRequest{
-		Type: veripassv1.ExitRequest_ExitType(mockPass1.Type),
-	}))
-	attest(t, err)
+		exitResponse, err := studentUserClient.Exit(ctx, connect.NewRequest(&veripassv1.ExitRequest{
+			Type: veripassv1.ExitRequest_ExitType(mockPass1.Type),
+		}))
+		attest(t, err)
 
-	mockPass1.Id = exitResponse.Msg.PassId
-	mockPass1.StartTime = timestamppb.Now()
+		mockPass1.Id = exitResponse.Msg.PassId
+		mockPass1.StartTime = timestamppb.Now()
 
-	_, err = studentUserClient.Entry(ctx, connect.NewRequest(&veripassv1.EntryRequest{
-		PassId: mockPass1.Id,
-	}))
-	attest(t, err)
+		_, err = studentUserClient.Entry(ctx, connect.NewRequest(&veripassv1.EntryRequest{
+			PassId: mockPass1.Id,
+		}))
+		attest(t, err)
 
-	mockPass1.EndTime = timestamppb.Now()
+		mockPass1.EndTime = timestamppb.Now()
 
-	exitResponse2, err := studentUserClient.Exit(ctx, connect.NewRequest(&veripassv1.ExitRequest{
-		Type: veripassv1.ExitRequest_ExitType(mockPass2.Type),
-	}))
-	attest(t, err)
+		exitResponse2, err := studentUserClient.Exit(ctx, connect.NewRequest(&veripassv1.ExitRequest{
+			Type: veripassv1.ExitRequest_ExitType(mockPass2.Type),
+		}))
+		attest(t, err)
 
-	mockPass2.Id = exitResponse2.Msg.PassId
-	mockPass2.StartTime = timestamppb.Now()
+		mockPass2.Id = exitResponse2.Msg.PassId
+		mockPass2.StartTime = timestamppb.Now()
 
-	pass, err := studentPassClient.GetPass(ctx, connect.NewRequest(&veripassv1.GetPassRequest{
-		PassId: mockPass1.Id,
-	}))
-	attest(t, err)
+		pass, err := studentPassClient.GetPass(ctx, connect.NewRequest(&veripassv1.GetPassRequest{
+			PassId: mockPass1.Id,
+		}))
+		attest(t, err)
 
-	failIfNotEqualPass(t, pass.Msg, &mockPass1, publicKey.Msg.PublicKey)
+		failIfNotEqualPass(t, pass.Msg, &mockPass1, publicKey.Msg.PublicKey)
 
-	pass2, err := studentPassClient.GetPass(ctx, connect.NewRequest(&veripassv1.GetPassRequest{
-		PassId: mockPass2.Id,
-	}))
-	attest(t, err)
+		pass2, err := studentPassClient.GetPass(ctx, connect.NewRequest(&veripassv1.GetPassRequest{
+			PassId: mockPass2.Id,
+		}))
+		attest(t, err)
 
-	failIfNotEqualPass(t, pass2.Msg, &mockPass2, publicKey.Msg.PublicKey)
+		failIfNotEqualPass(t, pass2.Msg, &mockPass2, publicKey.Msg.PublicKey)
 
-	latestPass, err := studentPassClient.GetLatestPassByUser(ctx, connect.NewRequest(&emptypb.Empty{}))
-	attest(t, err)
+		latestPass, err := studentPassClient.GetLatestPassByUser(ctx, connect.NewRequest(&emptypb.Empty{}))
+		attest(t, err)
 
-	failIfNotEqualPass(t, latestPass.Msg, &mockPass2, publicKey.Msg.PublicKey)
+		failIfNotEqualPass(t, latestPass.Msg, &mockPass2, publicKey.Msg.PublicKey)
 
-	pageToken := timestamppb.Now()
+		pageToken := timestamppb.Now()
 
-	passList1, err := studentPassClient.ListPassesByUser(ctx, connect.NewRequest(&veripassv1.ListPassesByUserRequest{
-		Type:      veripassv1.Pass_PASS_TYPE_UNSPECIFIED.Enum(),
-		PageToken: pageToken,
-		PageSize:  1,
-	}))
-	attest(t, err)
-	if passList1.Msg.NextPageToken.Seconds != pass.Msg.StartTime.Seconds {
-		t.Fatalf("Expected %v, got %v", pass.Msg.StartTime, passList1.Msg.NextPageToken)
-	}
-	failIfNotEqualPass(t, passList1.Msg.Passes[0], pass2.Msg, publicKey.Msg.PublicKey)
-	pageToken = passList1.Msg.NextPageToken
+		passList1, err := studentPassClient.ListPassesByUser(ctx, connect.NewRequest(&veripassv1.ListPassesByUserRequest{
+			Type:      veripassv1.Pass_PASS_TYPE_UNSPECIFIED.Enum(),
+			PageToken: pageToken,
+			PageSize:  1,
+		}))
+		attest(t, err)
+		failIfNotEqual(t, passList1.Msg.NextPageToken.Seconds, pass.Msg.StartTime.Seconds)
+		failIfNotEqualPass(t, passList1.Msg.Passes[0], pass2.Msg, publicKey.Msg.PublicKey)
+		pageToken = passList1.Msg.NextPageToken
 
-	passList2, err := studentPassClient.ListPassesByUser(ctx, connect.NewRequest(&veripassv1.ListPassesByUserRequest{
-		Type:      veripassv1.Pass_PASS_TYPE_UNSPECIFIED.Enum(),
-		PageToken: pageToken,
-		PageSize:  1,
-	}))
-	attest(t, err)
-	if passList2.Msg.NextPageToken != nil {
-		t.Fatal("Expected nil next page token")
-	}
-	failIfNotEqualPass(t, passList2.Msg.Passes[0], pass.Msg, publicKey.Msg.PublicKey)
+		passList2, err := studentPassClient.ListPassesByUser(ctx, connect.NewRequest(&veripassv1.ListPassesByUserRequest{
+			Type:      veripassv1.Pass_PASS_TYPE_UNSPECIFIED.Enum(),
+			PageToken: pageToken,
+			PageSize:  1,
+		}))
+		attest(t, err)
+		failIfNotEqual(t, passList2.Msg.NextPageToken, nil)
+		failIfNotEqualPass(t, passList2.Msg.Passes[0], pass.Msg, publicKey.Msg.PublicKey)
 
-	admin, err := adminAdminClient.GetAdmin(ctx, connect.NewRequest(&emptypb.Empty{}))
-	attest(t, err)
+		a, err := adminAdminClient.GetAdmin(ctx, connect.NewRequest(&emptypb.Empty{}))
+		attest(t, err)
+		failIfNotEqual(t, a.Msg, admin)
 
-	if !proto.Equal(admin.Msg, mockAdmin) {
-		t.Fatalf("Expected %v, got %v", &mockAdmin, admin.Msg)
-	}
+		hostelPassList1, err := adminAdminClient.GetAllPassesByHostel(ctx, connect.NewRequest(&veripassv1.GetAllPassesByHostelRequest{
+			Hostel:     admin.Hostel,
+			StartTime:  timestamppb.New(time.Unix(0, 0)),
+			EndTime:    timestamppb.Now(),
+			PassIsOpen: nil,
+			Type:       veripassv1.Pass_PASS_TYPE_UNSPECIFIED,
+			PageSize:   1,
+			PageToken:  timestamppb.Now(),
+		}))
+		attest(t, err)
+		failIfNotEqual(t, hostelPassList1.Msg.NextPageToken.Seconds, pass.Msg.StartTime.Seconds)
+		failIfNotEqual(t, hostelPassList1.Msg.Passes[0].StudentName, student.Name)
+		failIfNotEqual(t, hostelPassList1.Msg.Passes[0].StudentRoom, student.Room)
+		failIfNotEqualPass(t, hostelPassList1.Msg.Passes[0].Pass, pass2.Msg, publicKey.Msg.PublicKey)
 
-	hostelPassList1, err := adminAdminClient.GetAllPassesByHostel(ctx, connect.NewRequest(&veripassv1.GetAllPassesByHostelRequest{
-		Hostel:     mockAdmin.Hostel,
-		StartTime:  timestamppb.New(time.Unix(0, 0)),
-		EndTime:    timestamppb.Now(),
-		PassIsOpen: nil,
-		Type:       veripassv1.Pass_PASS_TYPE_UNSPECIFIED,
-		PageSize:   1,
-		PageToken:  timestamppb.Now(),
-	}))
-	attest(t, err)
-	if (hostelPassList1.Msg.NextPageToken == nil) || hostelPassList1.Msg.NextPageToken.Seconds != pass.Msg.StartTime.Seconds {
-		t.Fatalf("Expected %v, got %v", pass.Msg.StartTime, hostelPassList1.Msg.NextPageToken)
-	}
-	if hostelPassList1.Msg.Passes[0].StudentName != mockUser.Name {
-		t.Fatalf("Expected student name %v, got %v", mockUser.Name, hostelPassList1.Msg.Passes[0].StudentName)
-	}
-	if hostelPassList1.Msg.Passes[0].StudentRoom != mockUser.Room {
-		t.Fatalf("Expected student room %v, got %v", mockUser.Room, hostelPassList1.Msg.Passes[0].StudentRoom)
-	}
-	failIfNotEqualPass(t, hostelPassList1.Msg.Passes[0].Pass, pass2.Msg, publicKey.Msg.PublicKey)
+		hostelPassList2, err := adminAdminClient.GetAllPassesByHostel(ctx, connect.NewRequest(&veripassv1.GetAllPassesByHostelRequest{
+			Hostel:     admin.Hostel,
+			StartTime:  timestamppb.New(time.Unix(0, 0)),
+			EndTime:    timestamppb.Now(),
+			PassIsOpen: nil,
+			Type:       veripassv1.Pass_PASS_TYPE_UNSPECIFIED,
+			PageSize:   1,
+			PageToken:  hostelPassList1.Msg.NextPageToken,
+		}))
+		attest(t, err)
+		failIfNotEqual(t, hostelPassList2.Msg.NextPageToken, nil)
+		failIfNotEqual(t, hostelPassList2.Msg.Passes[0].StudentName, student.Name)
+		failIfNotEqual(t, hostelPassList2.Msg.Passes[0].StudentRoom, student.Room)
+		failIfNotEqualPass(t, hostelPassList2.Msg.Passes[0].Pass, pass.Msg, publicKey.Msg.PublicKey)
 
-	hostelPassList2, err := adminAdminClient.GetAllPassesByHostel(ctx, connect.NewRequest(&veripassv1.GetAllPassesByHostelRequest{
-		Hostel:     mockAdmin.Hostel,
-		StartTime:  timestamppb.New(time.Unix(0, 0)),
-		EndTime:    timestamppb.Now(),
-		PassIsOpen: nil,
-		Type:       veripassv1.Pass_PASS_TYPE_UNSPECIFIED,
-		PageSize:   1,
-		PageToken:  hostelPassList1.Msg.NextPageToken,
-	}))
-	attest(t, err)
-	if hostelPassList2.Msg.NextPageToken != nil {
-		t.Fatal("Expected nil next page token")
-	}
-	if hostelPassList2.Msg.Passes[0].StudentName != mockUser.Name {
-		t.Fatalf("Expected student name %v, got %v", mockUser.Name, hostelPassList2.Msg.Passes[0].StudentName)
-	}
-	if hostelPassList2.Msg.Passes[0].StudentRoom != mockUser.Room {
-		t.Fatalf("Expected student room %v, got %v", mockUser.Room, hostelPassList2.Msg.Passes[0].StudentRoom)
-	}
-	failIfNotEqualPass(t, hostelPassList2.Msg.Passes[0].Pass, pass.Msg, publicKey.Msg.PublicKey)
-
-	outCount, err := adminAdminClient.GetOutCountByHostel(ctx, connect.NewRequest(&veripassv1.GetOutCountByHostelRequest{
-		Hostel:    mockAdmin.Hostel,
-		StartTime: timestamppb.New(time.Unix(0, 0)),
-		EndTime:   timestamppb.Now(),
-		Type:      veripassv1.Pass_PASS_TYPE_UNSPECIFIED,
-	}))
-	attest(t, err)
-	if outCount.Msg.Out != 1 {
-		t.Fatalf("Expected 1 out count, got:%v", outCount.Msg.Out)
-	}
+		outCount, err := adminAdminClient.GetOutCountByHostel(ctx, connect.NewRequest(&veripassv1.GetOutCountByHostelRequest{
+			Hostel:    admin.Hostel,
+			StartTime: timestamppb.New(time.Unix(0, 0)),
+			EndTime:   timestamppb.Now(),
+			Type:      veripassv1.Pass_PASS_TYPE_UNSPECIFIED,
+		}))
+		attest(t, err)
+		failIfNotEqual(t, outCount.Msg.Out, 1)
+	})
 }
 
 func attest(t *testing.T, err error) {
@@ -236,13 +215,28 @@ func attest(t *testing.T, err error) {
 	}
 }
 
+func failIfNotEqual[T comparable](t *testing.T, got T, expected T) {
+	t.Helper()
+	failed := false
+	switch any(got).(type) {
+	case proto.Message:
+		failed = !proto.Equal(any(got).(proto.Message), any(expected).(proto.Message))
+	default:
+		failed = (got != expected)
+	}
+
+	if failed {
+		t.Fatalf("Expected: %v\nGot:%v", expected, got)
+	}
+}
+
 func failIfNotEqualPass(t *testing.T, got *veripassv1.Pass, expected *veripassv1.Pass, publicKey ed25519.PublicKey) {
 	t.Helper()
 	verifyPass(t, got, publicKey)
-	if got.Id != expected.Id ||
-		got.UserId != expected.UserId ||
-		got.Type != expected.Type ||
-		math.Abs(float64(got.StartTime.Seconds-expected.StartTime.Seconds)) > 1 ||
+	failIfNotEqual(t, got.Id, expected.Id)
+	failIfNotEqual(t, got.UserId, expected.UserId)
+	failIfNotEqual(t, got.Type, expected.Type)
+	if math.Abs(float64(got.StartTime.Seconds-expected.StartTime.Seconds)) > 1 ||
 		(expected.EndTime != nil && math.Abs(float64(got.EndTime.Seconds-expected.EndTime.Seconds)) > 1) {
 		t.Fatalf("Expected %v, got %v", expected, got)
 	}
@@ -270,14 +264,10 @@ func verifyPass(t *testing.T, pass *veripassv1.Pass, publicKey ed25519.PublicKey
 	}
 
 	passID := string(signedQrCode[:36])
-	if passID != pass.Id {
-		t.Fatalf("Expected pass ID %v, got %v", pass.Id, passID)
-	}
+	failIfNotEqual(t, passID, pass.Id)
 
 	userID := string(signedQrCode[37:secondDelimemter])
-	if userID != pass.UserId {
-		t.Fatalf("Expected user ID %v, got %v", pass.UserId, userID)
-	}
+	failIfNotEqual(t, userID, pass.UserId)
 }
 
 func getClient(t *testing.T, email string) *http.Client {
